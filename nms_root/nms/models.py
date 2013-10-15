e10172a845e2c6da1c285e5b50ba770dd61a594f
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-10-15 10:25:06 +0200

    Added __str__ methods to nms models and changed the roles table primary key

--- nms_root/nms/models.py
+++ nms_root/nms/models.py
@@ -5,10 +5,16 @@ from django.db import models
 class vendor(models.Model):
 	vendor_id = models.AutoField(primary_key=True)
 	vendor_name = models.CharField(max_length=255)
+	
+	def __str__(self):
+		return self.vendor_name
 
 class os_type(models.Model):
 	os_type_id = models.AutoField(primary_key=True)
 	type = models.CharField(max_length=255)
+	
+	def __str__(self):
+		return self.type
 
 class os(models.Model):
 	os_id = models.AutoField(primary_key=True)
@@ -17,27 +23,42 @@ class os(models.Model):
 	build = models.CharField(max_length=255)
 	short_info = models.CharField(max_length=255)
 	name = models.CharField(max_length=255)
+	
+	def __str__(self):
+		return self.name + ' ' + self.build
 
 class dev_model(models.Model):
 	model_id = models.AutoField(primary_key=True)
 	model_name = models.CharField(max_length=255)
 	version = models.CharField(max_length=255)
+	
+	def __str__(self):
+		return self.model_name + ' ' + self.version
 
 class dev_type(models.Model):
 	dev_type_id = models.AutoField(primary_key=True)
 	dev_type_name = models.CharField(max_length=255)
+	
+	def __str__(self):
+		return self.dev_type_name
 
 class gen_dev(models.Model):
 	gen_dev_id = models.AutoField(primary_key=True)
 	vendor_id = models.ForeignKey(vendor)
 	model_id = models.ForeignKey(dev_model)
 	dev_type_id = models.ForeignKey(dev_type)
+	
+	def __str__(self):
+		return self.vendor_id + ' ' + self.model_id #Making use of the defined __str__ methods of these classes
 
 class os_dev(models.Model):
 	os_dev_id = models.AutoField(primary_key=True)
 	os_id = models.ForeignKey(os)
 	gen_dev_id = models.ForeignKey(gen_dev)
 
+	def __str__(self):
+		return self.os_id + ' ' + self.gen_dev_id
+	
 class devices(models.Model):
 	dev_id = models.AutoField(primary_key=True)
 	gen_dev_id = models.ForeignKey(gen_dev)
@@ -49,10 +70,17 @@ class devices(models.Model):
 	password_remote = models.CharField(max_length=255)
 	password_enable = models.CharField(max_length=255)
 	pref_remote_prot = models.CharField(max_length=255, default='ssh')
+	
+	def __str__(self):
+		return self.gen_dev_id + ' - ' + self.ip
 
 class roles(models.Model):
-	user_id = models.AutoField(primary_key=True)
+	roles_id = models.AutoField(primary_key=True)
+	user_id = models.ForeignKey(user)
 	dev_id = models.ForeignKey(devices)
+
+	def __str__(self):
+		return self.user_id + ': ' + self.dev_id
 
 class user(models.Model):
 	user_id = models.AutoField(primary_key=True)
@@ -62,6 +90,6 @@ class user(models.Model):
 	password = models.CharField(max_length=255)
 	is_active = models.BooleanField(default=False)
 	mode = models.PositiveIntegerField(default = '0')
-
 	
-
+	def __str__(self):
+		return self.username
