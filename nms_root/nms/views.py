5415dd8dca823ebae85f0048694e6bdb10779812
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-10-16 19:19:23 +0200

    Some changes are made in the template add_device and views are updated.

--- nms_root/nms/views.py
+++ nms_root/nms/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from nms.models import *
+from django.contrib import messages
 
 
 def index(request):
@@ -17,13 +18,19 @@ def nms_admin_login(request):
 	return render(request, 'nms/login.html')
 
 def nms_admin_add_device(request):
+	dev_type_view = Dev_type.objects.all()
+	vendor_view = Vendor.objects.all()
+	dev_model_view = Dev_model.objects.all()
+	os_view = OS.objects.all()
 	if request.method == 'POST':
 		#return HttpResponse('Received post method.')
 		q = Devices()
+		
+		
 		try:
 			dev_type = request.POST['dev_type']
-			vendor_name = request.POST['vendor_name']
-			dev_model = request.POST['dev_model']
+			vendor = Vendor.objects.filter(vendor_name = request.POST['vendor_name'])
+			dev = Dev_model.objects.filter(model_name = request.POST['dev_model'])
 			os_name = request.POST['os_name']
 			pref_remote_prot = request.POST['pref_remote_prot']
 			ipprot = request.POST['ipprot']
@@ -33,9 +40,11 @@ def nms_admin_add_device(request):
 			password_remote = request.POST['password_remote']
 			password_enable = request.POST['password_enable']
 		except KeyError:
-				return render(request, 'nms/add_device.html', {'error_message': 'Not all fields are set'})
+			messages.error(request, 'Not all fields are set')
+			return HttpResponseRedirect(reverse('nms:nms_admin_add_device'))
 		
 		
+		messages.info(request, 'Database updated')
 		return HttpResponseRedirect(reverse('nms:nms_admin_add_device'))
 	else:
-		return render(request, 'nms/add_device.html')
+		return render(request, 'nms/add_device.html', {'dev_type_view': dev_type_view, 'vendor_view': vendor_view, 'dev_model_view' : dev_model_view, 'os_view': os_view})
