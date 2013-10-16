79ba25d5ee5aafba056043ae93ee22bcbbec9fe2
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-10-16 18:56:04 +0200

    Added namespace.

--- nms_root/nms_root/urls.py
+++ nms_root/nms_root/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nms_root.views.home', name='home'),
     # url(r'^nms_root/', include('nms_root.foo.urls')),
-     url(r'^', include('nms.urls')),
+     url(r'^', include('nms.urls', namespace = 'nms')),
     # Uncomment the admin/doc line below to enable admin documentation:
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
