d9e9b266c3a198b74e517bd855ca5014dfcb418d
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-11-22 15:05:12 +0100

    Debugging.

--- unstable/nms_root/nms/views.py
+++ unstable/nms_root/nms/views.py
@@ -123,7 +123,7 @@ def acl_handler(request, acl_id):
                 
         except:
             messages.error(request, 'Not all required fields are set')
-            messages.error(request, traceback.print_exc()) #debug code
+            messages.error(request, traceback.format_exc()) #debug code
             messages.error(request, list(request.POST.items())) #debug code
             return HttpResponseRedirect(reverse('nms:acl_groups'))
     else:
