760706329c54e22fb90d68e8a9f5cf249a67aedb
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-11-22 15:50:36 +0100

    Checked for templates is now added.

--- unstable/nms_root/nms/views.py
+++ unstable/nms_root/nms/views.py
@@ -136,8 +136,10 @@ def acl_handler(request, acl_id):
 def acl_groups_manage(request, acl_id):
     group = get_object_or_404(Group, pk=acl_id)
     devices = Devices.objects.all()
-    
-    return render(request, 'nms/acl_groups_manage.html', {'devices': devices, 'group':group })
+    list_check = 'checked' if group.permissions.filter(codename='list_devices').exists() else ''
+    manage_check = 'checked' if group.permissions.filter(codename='manage_devices').exists() else ''
+    change_check = 'checked' if group.permissions.filter(codename='change_devices').exists() else ''
+    return render(request, 'nms/acl_groups_manage.html', {'devices': devices, 'group':group, 'list_check': list_check, 'manage_check': manage_check, 'change_check': change_check })
 
 @login_required
 def acl_groups_add(request):
