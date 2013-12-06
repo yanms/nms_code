fe3b066dc8a6bd3bce4797f8e19d845145661fe9
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-12-06 15:27:25 +0100

    Added debugging.

--- unstable/nms_root/nms/views.py
+++ unstable/nms_root/nms/views.py
@@ -606,6 +606,7 @@ def device_modify(request, device_id_request):
 					if len(model_id) == 1:
 						device.gen_dev = Gen_dev.objects.get(model_id=model_id[0], vendor_id=Vendor.objects.get(vendor_name=request.POST['selectVendor']), dev_type_id=Dev_type.objects.get(dev_type_name=request.POST['selectType']))
 					else:
+						messages.error(request, list(request.POST.items()))
 						messages.error(request, "Received multiple models, not unique")
 						return HttpResponseRedirect(reverse('nms:device_modify', args=(device.dev_id,)))
 				except:
