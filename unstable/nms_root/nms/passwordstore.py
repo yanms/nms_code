9631bb3a3f13d871498bf8e1bcf2cc91bf253949
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-12-06 11:45:17 +0100

    New passwordstore

--- unstable/nms_root/nms/passwordstore.py
+++ unstable/nms_root/nms/passwordstore.py
@@ -12,13 +12,13 @@ class AESCipher:
 		self.BS = 16
 		self.pad = lambda s: s + (self.BS - len(s) % self.BS) * chr(self.BS - len(s) % self.BS)
 		self.unpad = lambda s: s[0:-s[-1]]
-	
+
 	def encrypt(self, raw):
 		raw = self.pad(raw)
 		iv = Random.new().read(AES.block_size)
 		cipher = AES.new(self.key, AES.MODE_CBC, iv)
 		return base64.b64encode(iv+cipher.encrypt(raw))
-	
+
 	def decrypt(self, enc):
 		enc = base64.b64decode(enc)
 		iv = enc[:16]
@@ -28,7 +28,7 @@ class AESCipher:
 def storeMasterPassword(password):
 	global masterPasswordLock
 	global masterPassword
-	if len(password) != 16 and len(password) != 24 and len(password) != 32:
+	if len(password) != 16:
 		return -1
 	masterPasswordLock.acquire()
 	masterPassword = password
@@ -47,26 +47,28 @@ def hasMasterPassword():
 		masterPasswordLock.release()
 
 def storeEnablePassword(device, password):
-	cipher = AESCipher(masterPassword)
-	device.password_enable = cipher.encrypt(password)
+	salt = base64.b64encode(Random.new().read(16))[:16].decode()
+	cipher = AESCipher(masterPassword + salt)
+	device.password_enable = salt + '$' + cipher.encrypt(password).decode()
 	device.save()
 
 def storeRemotePassword(device, password):
+    salt = base64.b64encode(Random.new().read(16))[:16].decode()
 	cipher = AESCipher(masterPassword)
-	device.password_remote = cipher.encrypt(password)
+	device.password_remote = salt + '$' + cipher.encrypt(password).decode()
 	device.save()
 
 def getEnablePassword(device):
 	try:
-		cipher = AESCipher(masterPassword)
-		return cipher.decrypt(device.password_enable)
+		cipher = AESCipher(masterPassword + device.password_enable[:device.password_enable.find('$')])
+		return cipher.decrypt(device.password_enable[device.password_enable.find('$')+1:])
 	except:
 		return b''
 		
 
 def getRemotePassword(device):
 	try:
-		cipher = AESCipher(masterPassword)
-		return cipher.decrypt(device.password_remote)
+		cipher = AESCipher(masterPassword + device.password_remote[:device.password_remote.find('$')])
+		return cipher.decrypt(device.password_remote[device.password_remote.find('$')+1:])
 	except:
 		return b''
