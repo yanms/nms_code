4f8b188bf99b020575c65b3e3471f552c7966bfe
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-12-13 14:02:16 +0100

    Bugfix.

--- unstable/nms_root/nms/views.py
+++ unstable/nms_root/nms/views.py
@@ -233,7 +233,7 @@ def acl_handler(request, acl_id):
 					for iter in groups:
 						group = get_object_or_404(Group, name=iter)
 						user_obj.groups.add(group)
-						History.objects.create(action_type = 'Modified user groups. Currect user groups are: {0}'.format(iter), date_time = timezone.now())
+						History.objects.create(action_type = 'Modified user groups. Currect user groups are: {0}'.format(iter))
 					messages.success(request, 'Database updated successfully')
 					return HttpResponseRedirect(reverse('nms:acl_user'))
 				else:
