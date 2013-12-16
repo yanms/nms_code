5c91c1fd8d0d6c717e736b12650aee8deaf0e859
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2013-12-16 11:42:29 +0100

    You can now remove a user from the current session.

--- unstable/nms_root/nms/views.py
+++ unstable/nms_root/nms/views.py
@@ -16,6 +16,7 @@ import  django.db.models as django_exception
 from django.utils import timezone
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.sessions.models import Session
 
 
 @login_required
@@ -1157,3 +1158,13 @@ def acl_device_history(request, acl_id):
 		return render(request, 'nms/acl_devices_history.html', {'request': request, 'history': history_items})
 	else:
 		return HttpResponseRedirect(reverse('nms:acl'))
+
+@login_required
+def acl_kick_user(request, user_id):
+	if request.user.has_perm('auth.change_user'):
+		user_obj = get_object_or_404(User, pk=user_id)
+		[x.delete() for x in Session.objects.all() if x.get_decoded().get('_auth_user_id') == user_obj.id]
+		messages.success(request, 'User {0} is kicked out of the current sessions.'.format(user_obj))
+		return HttpResponseRedirect(reverse('nms:acl_user'))
+	else:
+		return HttpResponseRedirect(reverse('nms:index'))
