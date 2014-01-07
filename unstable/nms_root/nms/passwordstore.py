549d073e785e1c116543ccd22670eb510a8c0da2
Sebastiaan Groot <sebastiaangroot@gmail.com>
2014-01-07 16:29:21 +0100

    always calling initshmlib

--- unstable/nms_root/nms/passwordstore.py
+++ unstable/nms_root/nms/passwordstore.py
@@ -45,8 +45,8 @@ def storeMasterPassword(password):
 
 def hasMasterPassword():
 	global lib
-	if lib == None:
-		__initSHMLib__()
+	#if lib == None:
+	__initSHMLib__()
 
 	if len(lib.get_password()) == 16:
 		return True
