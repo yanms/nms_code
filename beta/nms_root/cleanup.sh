56246351a2739681316b925d25698f12dc095773
Sebastiaan Groot <sebastiaangroot@gmail.com>
2014-01-08 09:38:40 +0100

    Added first comments

--- beta/nms_root/cleanup.sh
+++ beta/nms_root/cleanup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# A cleanup script to remove the SHM and SEM created by the application if needed
+#
+# Copyright (c) 2014 Remy Bien, Sebastiaan Groot, Wouter Miltenburg and Koen Veelenturf
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option) 
+# any later version.
+
+
 SHMKEY=0x000017de
 SEMKEY=NMSSemKey
 
