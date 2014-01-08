be522214c66a0302c81941e1678f3722b189eba7
Sebastiaan Groot <sebastiaangroot@gmail.com>
2014-01-08 10:57:07 +0100

    Added comments to shmlib.c

--- beta/nms_root/nms/c/shmlib.c
+++ beta/nms_root/nms/c/shmlib.c
@@ -1,3 +1,14 @@
+/*
+ * A small library used in passwordstore.py to access shared memory
+ *
+ * Copyright (c) 2014 Remy Bien, Sebastiaan Groot, Wouter Miltenburg and Koen Veelenturf
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ */
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
