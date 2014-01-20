7e5a3bc040ea4cf233f0b5011740823dbb6d8f87
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2014-01-20 10:16:26 +0100

    Updated DB.

--- src/nms_root/nms_root/settings.py
+++ src/nms_root/nms_root/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/var/www/django/nms_code/beta/nms_root/nms.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'nms.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
