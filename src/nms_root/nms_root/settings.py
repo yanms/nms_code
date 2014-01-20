c49efeb0c8097e5d86587febe5b3cbc2ee65a172
Wouter Miltenburg <wouter.miltenburg@hva.nl>
2014-01-20 10:33:31 +0100

    Updated settings.py

--- src/nms_root/nms_root/settings.py
+++ src/nms_root/nms_root/settings.py
@@ -3,7 +3,7 @@ import os
 
 SETTINGS_PATH = os.path.dirname(os.path.realpath(__file__))
 SETTINGS_PATH = SETTINGS_PATH[:SETTINGS_PATH.rfind('nms_root')]
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -25,7 +25,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
